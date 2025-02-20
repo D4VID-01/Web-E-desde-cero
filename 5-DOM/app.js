fdc0c0ebbd02212f4ba2f38c3fd1b48ecf64e2f9
Luis David <luisdapm007@egmail.com>
2025-02-19 19:38:37 -0600

    Función que almacena en un Array

--- 5-DOM/app.js
+++ 5-DOM/app.js
@@ -1,4 +1,20 @@
 
-function holaMundo(){
-    alert('Hola Mundo!');
+let listaDeComentarios =[];
+
+//Función que empuja los comentarios a un array
+function agregarComentarios(){
+    let comentario = document.getElementById('comentarioUsuario');
+
+    if(comentario.value===''){
+        comentario.placeholder='Este campo debe ser llenado antes...'
+    } else{
+        listaDeComentarios.push(comentario.value)
+        comentario.value='';
+        comentario.placeholder='Escribe tu comentario aquí...';
+    }
+}
+
+//Función que muestra los comentarios en el DOM
+function mostratComentario(){
+
 }
